38fd6106e922a18a94e17d0319a04e6a6cae4442
delete1212 <127916444+delete1212@users.noreply.github.com>
2024-06-20 13:05:36 +0900

    240620

--- java/src/g1/MainGame.java
+++ java/src/g1/MainGame.java
@@ -1,8 +1,17 @@
 package g1;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MainGame extends JFrame {
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
 	
 	public MainGame() {
 		setTitle("Battle Game");
@@ -11,5 +20,22 @@ public class MainGame extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../image/2109679.jpg")).getImage();
+		
+		Music introMusic = new Music(true);
+		introMusic.start();
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage,  0 ,  0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
 	}
 }
