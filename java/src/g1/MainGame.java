6944a0c4d86e78ab3e279eac3e7a5e1d5633ad7c
delete1212 <127916444+delete1212@users.noreply.github.com>
2024-06-26 14:41:00 +0900

    240626

--- java/src/g1/MainGame.java
+++ java/src/g1/MainGame.java
@@ -7,6 +7,7 @@ import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,13 +21,38 @@ public class MainGame extends JFrame {
 	
 	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../image/261420-300.png"));
 	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../image/261420-200.png"));
+	private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("../image/startButtonEnter.png"));
+	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../image/startButtonBasic.png"));
+	private ImageIcon endButtonEnteredImage = new ImageIcon(Main.class.getResource("../image/endButtonEnter.png"));
+	private ImageIcon endButtonBasicImage = new ImageIcon(Main.class.getResource("../image/endButtonBasic.png"));
+	private ImageIcon leftButtonBasic = new ImageIcon(Main.class.getResource("../image/Left.png"));
+	private ImageIcon leftButtonEntered = new ImageIcon(Main.class.getResource("../image/Left.png"));
+	private ImageIcon rightButtonBasic = new ImageIcon(Main.class.getResource("../image/Right.png"));
+	private ImageIcon rightButtonEntered = new ImageIcon(Main.class.getResource("../image/Right.png"));
+
 	
-	private Image introBackground = new ImageIcon(Main.class.getResource("../image/2109679.jpg")).getImage();
+
+	
+	private Image background = new ImageIcon(Main.class.getResource("../image/2109679.jpg")).getImage();
 	private JLabel menuBar  = new JLabel(new ImageIcon(Main.class.getResource("../image/menuBar.png")));
+	
 	private JButton exitButton = new JButton(exitButtonBasicImage);
+	private JButton startButton = new JButton(startButtonBasicImage);
+	private JButton endButton = new JButton(endButtonBasicImage);
+	private JButton leftButton = new JButton(leftButtonBasic);
+	private JButton rightButton = new JButton(rightButtonBasic);
 	
 	
 	private int mouseX, mouseY;
+	
+	private boolean isMainScreen = false;
+	
+	ArrayList<Track> trackList = new ArrayList<Track>();
+	
+	private Image titleImage;
+	private Image selectedImage;
+	private Music selectedMusic;
+	private int nowSelected = 0;
 	
 	public MainGame() {
 		setUndecorated(true);
@@ -39,8 +65,13 @@ public class MainGame extends JFrame {
 		setBackground(new Color(0,0,0,0));
 		setLayout(null);
 		
-		Music introMusic = new Music("Pyrite.mp3", true);
+		Music introMusic = new Music("Title.mp3", true);
 		introMusic.start();
+		
+		trackList.add(new Track("cc1Title.png", "cc1Start.jpg", "cc1game.png", "Pyrite.mp3", "Pyrite.mp3"));
+		trackList.add(new Track("cc2Title.png", "cc2Start.jpg", "cc2game.jpg", "Blade.mp3", "Blade.mp3"));
+		trackList.add(new Track("cc4Title.png", "cc4Start.jpg", "cc4game.jpg", "LeadSeal.mp3", "LeadSeal.mp3"));
+		
 		
 		exitButton.setBounds(1245, 0, 30, 30);
 		exitButton.setBorderPainted(false);
@@ -50,16 +81,12 @@ public class MainGame extends JFrame {
 			public void mouseEntered(MouseEvent e) {
 				exitButton.setIcon(exitButtonEnteredImage);
 				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-				Music buttonEnteredMusic = new Music("Blade.mp3", false);
-				buttonEnteredMusic.start();
 			}
 			public void mouseExited(MouseEvent e) {
 				exitButton.setIcon(exitButtonBasicImage);
 				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			public void mousePressed(MouseEvent e) {
-				Music buttonEnteredMusic = new Music("LeadSeal.mp3", false);
-				buttonEnteredMusic.start();
 				try {
 					Thread.sleep(1000);
 				}	catch(InterruptedException ex) {
@@ -70,6 +97,58 @@ public class MainGame extends JFrame {
 		});
 		
 		add(exitButton);
+		
+		startButton.setBounds(40, 200, 400, 100);
+		startButton.setBorderPainted(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setFocusPainted(false);
+		startButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				startButton.setIcon(startButtonEnteredImage);
+				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				startButton.setIcon(startButtonBasicImage);
+				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				introMusic.close();
+				selectTrack(0);
+				startButton.setVisible(false);
+				endButton.setVisible(false);
+				leftButton.setVisible(true);
+				rightButton.setVisible(true);
+				background = new ImageIcon(Main.class.getResource("../image/76043793_p0.jpg")).getImage();
+				isMainScreen = true;
+			}
+		});
+		
+		add(startButton);
+		
+		endButton.setBounds(40, 330, 400, 100);
+		endButton.setBorderPainted(false);
+		endButton.setContentAreaFilled(false);
+		endButton.setFocusPainted(false);
+		endButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				endButton.setIcon(endButtonEnteredImage);
+				endButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				endButton.setIcon(endButtonBasicImage);
+				endButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				try {
+					Thread.sleep(1000);
+				}	catch(InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		
+		add(endButton);
 		
 		menuBar.setBounds(0,0,1280,30);
 		menuBar.addMouseListener(new MouseAdapter() {
@@ -92,6 +171,46 @@ public class MainGame extends JFrame {
 		});
 		add(menuBar);
 		
+		leftButton.setVisible(false);
+		leftButton.setBounds(140, 310, 60, 60);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEntered);
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasic);
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				selectedLeft();
+				
+			}
+		});
+		add(leftButton);
+		
+		rightButton.setVisible(false);
+		rightButton.setBounds(1080, 310, 60, 60);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(leftButtonEntered);
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(leftButtonBasic);
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				selectedRight();
+			}
+		});
+		add(rightButton);
 
 	}
 	
@@ -103,8 +222,36 @@ public class MainGame extends JFrame {
 	}
 	
 	public void screenDraw(Graphics g) {
-		g.drawImage(introBackground, 0, 0, null);
+		g.drawImage(background, 0, 0, null);
+		if(isMainScreen) {
+			g.drawImage(selectedImage, 340, 100, null);
+			g.drawImage(titleImage, 300, 40, null);
+		}
 		paintComponents(g);
 		this.repaint();
+	}
+	
+	public void selectTrack(int nowSelected) {
+		if(selectedMusic != null)
+			selectedMusic.close();
+		titleImage = new ImageIcon(Main.class.getResource("../image/" + trackList.get(nowSelected).getTitleImage())).getImage();
+		selectedImage = new ImageIcon(Main.class.getResource("../image/" + trackList.get(nowSelected).getStartImage())).getImage();
+		selectedMusic = new Music(trackList.get(nowSelected).getStartMusic(), true);
+		selectedMusic.start();
+	}
+	
+	public void selectedLeft() {
+		if(nowSelected == 0)
+			nowSelected = trackList.size() - 1;
+		else
+			nowSelected--;
+		selectTrack(nowSelected);
+	}
+	public void selectedRight() {
+		if(nowSelected == trackList.size() - 1)
+			nowSelected = 0;
+		else
+			nowSelected++;
+		selectTrack(nowSelected);
 	}
 }
